4749980d5c8a1457224b6c29a9d8ed33fc12bfe6
PRIYTESH PRASAD <61977852+priyteshprasad@users.noreply.github.com>
2024-04-20 12:57:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make = make
+	this.model = model
+}
+Car.prototype.getMakeModel = function(){
+	return this.make + " " + this.model
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	Car.call(this, make, model)
+	this.topSpeed = topSpeed;
+	
+}
+Car.prototype.getTopSpeed = function(){
+	return this.topSpeed
+}
+Object.setPrototypeOf(SportsCar.prototype, Car.prototype)
+
+// const car = new SportsCar("Ferrari", "Testarossa", 200);
+// console.log(car.getMakeModel()); // Output: Ferrari Testarossa
+// console.log(car.getTopSpeed()); // Output: 200
 
 // Do not change the code below
 window.Car = Car;
